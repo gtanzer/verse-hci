8b7b70c020e9db08943ce24cfc5a5c7783e56b6c
Garrett Tanzer <gtanzer@college.harvard.edu>
2019-12-04 08:18:27 -0500

    compiling

--- ocaml2prose/compile.ml
+++ ocaml2prose/compile.ml
@@ -42,7 +42,7 @@ let cmp_operator names (ity, in_name : ty * string) (o : operator) : string opti
     | _ -> None
     end
 
-let cmp_rule names (ity, in_name : ty * string) (t : ty) (o : Rule.t) : string =
+let cmp_rule names (ity, in_name : ty * string) (t : ty) (o : Rule.t) : string * (ty list) =
     let decl = Printf.sprintf "%s %s" (string_of_ty t) (TMap.find t names) in
     let constrs = List.map (cmp_operator names (ity, in_name)) (Rule.elements o) in
     let constrs' = List.fold_left (fun l c -> match c with
@@ -50,18 +50,20 @@ let cmp_rule names (ity, in_name : ty * string) (t : ty) (o : Rule.t) : string =
                                              | _ -> l ) [] constrs in
     let constrs'' = if ity = t then in_name :: constrs' else constrs' in
     begin match constrs'' with
-    | [] -> decl ^ ";"
-    | _ -> decl ^ " := " ^ (String.concat " | " constrs'') ^ ";"
+    | [] -> (decl ^ ";"), [t]
+    | _ -> (decl ^ " := " ^ (String.concat " | " constrs'') ^ ";"), []
     end
 
-let cmp_grammar (ity, oty, d : dsl) : string =
+let cmp_grammar (ity, oty, d : dsl) : string * (ty * string) list =
     let header = read_file (headerpath ^ "/grammar") in
     let in_name = name() in
     let input = (Printf.sprintf "@input %s %s;\n@start" (string_of_ty ity) in_name) in
     let names = TMap.fold (fun t _ m -> TMap.add t (name()) m) d TMap.empty in
-    let body = TMap.fold (fun t o ss -> (cmp_rule names (ity, in_name) t o)::ss) d [] in
+    let body2 = TMap.fold (fun t o ss -> (cmp_rule names (ity, in_name) t o)::ss) d [] in
+    let body, priml = List.split body2 in
+    let prims = List.map (fun t -> t, (TMap.find t names)) (List.flatten priml) in
     let footer = read_file (footerpath ^ "/grammar") in
-    (String.concat "\n" ([header; input] @ body @ [footer]))
+    (String.concat "\n" ([header; input] @ body @ [footer])), prims
 
 let cmp_other (ids : string list) (p : string) : string =
     let header = read_file (headerpath ^ "/" ^ p) in
@@ -70,12 +72,26 @@ let cmp_other (ids : string list) (p : string) : string =
     let footer = read_file (footerpath ^ "/" ^ p) in
     String.concat "\n" (header :: body @ [footer])
 
+let cmp_prim_scores (tss : (ty * string) list) : string =
+    let ss = List.map (fun (t, s) ->
+        Printf.sprintf "[FeatureCalculator(\"%s\", Method = CalculationMethod.FromLiteral)]\n
+                        public static double %s(%s %s) => -1.0;" s (String.uppercase_ascii s) (string_of_ty t) s
+    ) tss in
+    String.concat "\n" ss
+
+let cmp_scores (ids : string list) (p : string) (tss : (ty * string) list): string =
+    let header = read_file (headerpath ^ "/" ^ p) in
+    let ppath = libpath ^ "/" ^ p ^ "/" in
+    let body = List.map (fun n -> read_file (ppath ^ n)) ids in
+    let footer = read_file (footerpath ^ "/" ^ p) in
+    String.concat "\n" (header :: body @ [cmp_prim_scores tss; footer])
+
 let cmp_dsl (ity, oty, d : dsl) : unit =
-    let grammar = cmp_grammar (ity, oty, d) in
+    let grammar, prims = cmp_grammar (ity, oty, d) in
     let ids = TMap.fold (fun _ os l -> Rule.fold (fun o l -> match o with OOp0 i -> i::l | _ -> l) os l) d [] in
     let semantics = cmp_other ids "semantics" in
     let witnesses = cmp_other ids "witnesses" in
-    let scores = cmp_other ids "scores" in
+    let scores = cmp_scores ids "scores" prims in
     let configpath = libpath ^ "/config/" in
     let program = read_file (configpath ^ "Program.cs") in
     let verse = read_file (configpath ^ "Verse.csproj") in
