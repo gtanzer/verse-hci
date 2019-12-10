6ecd0a7060fa67e2c4107c0ba71e05b9ae528810
Alex Wendland <me@alexwendland.com>
2019-12-10 16:46:08 -0500

    summative: Cleanup verse.sh, add summative script, and provide a better corpus example

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # verse
+
+A synthesizer for synthesizers built on top of the PROSE framework. TODO: expand on this more.
+
+## Usage
+
+Run `verse.sh` on a corpus file (see below for an example). A PROSE configuration will be generated and you will be dropped into an interactive shell with the resulting synthesizer.
+
+e.g. `verse.sh corpus.txt`
+
+## Example
+
+Provide the following `corpus.txt`:
+
+```
+@input string x
+@output string
+
+append(x, x)
+```
+
+## Misc.
+
+Tested using ocaml 4.08.1. No opam libraries required.
+
+Tested using dotnet-core 3.0.1. Additional dotnet libraries are required and should be autoinstalled (check the resulting Verse.csproj file in `output/`).
--- ocaml2prose/verse.sh
+++ ocaml2prose/verse.sh
@@ -1,10 +1,28 @@
+#! /bin/bash
+set -e
+
+BIN_DIR=$(realpath "$(dirname "$0")")
+
 if [ "$#" -ne 1 ]; then
-    FILE="corpus.txt"
-    echo "using default corpus location," $FILE
-else FILE=$1
+    REL_FILE="$BIN_DIR/corpus.txt"
+    echo "using default corpus location: $REL_FILE"
+else REL_FILE=$1
 fi
-make clean \
-&& make; \
-(mkdir output; mkdir output/synthesis; mkdir output/synthesis/grammar); \
-./main.native $FILE \
-&& (cd output && dotnet build && dotnet run)
+
+ABS_FILE=$(realpath "$REL_FILE")
+
+(
+    # Keep everything local to the script's directory
+    # since the ocaml2prose program uses paths that are relative
+    # to the present working directory, not to its script location
+    cd "$BIN_DIR"
+    make clean
+    make
+    mkdir -p output/synthesis/grammar
+    "$BIN_DIR/main.native" "$ABS_FILE"
+    (
+        cd output
+        dotnet build
+        dotnet run
+    )
+)
